4a34ece5e06a6efd13f21bb6b1aad11c791fd82e
mbok <mboook@gmail.com>
2018-03-19 23:19:48 +0100

    update

--- control/control.py
+++ control/control.py
@@ -117,7 +117,8 @@ def on_message(client, userdata, msg):
     servoHead.set_trim(float(msg.payload))
   elif "speach/say" in msg.topic:
     text = msg.payload.decode("utf-8")
-    textHash = speachLanguage + "-" + hashlib.md5(text.encode()).hexdigest()
+    #textHash = speachLanguage + "-" + hashlib.md5(text.encode()).hexdigest()
+    textHash="j"
     file = "/tmp/speach+"+str(textHash)+".mp3"
     logger.debug("File for speach: " + file)
     if not os.path.isfile(file):
