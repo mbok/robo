2dc96ad98a1e2aa6c3aea43916103096822a616d
mbok <mboook@gmail.com>
2018-04-01 23:02:50 +0200

    update

--- control/control.py
+++ control/control.py
@@ -1,16 +1,7 @@
 from threading import Thread
-from gtts import gTTS
-
 import logging
 import random
 import time
-import hashlib
-import os.path
-
-import pygame as pg
-
-import urllib
-
 import RPi.GPIO as GPIO
 import paho.mqtt.client as mqtt
 
@@ -26,37 +17,6 @@ GPIO_ECHO = 24
 GPIO.setup(GPIO_TRIGGER, GPIO.OUT)
 GPIO.setup(GPIO_ECHO, GPIO.IN)
 
-# Init music
-freq = 44100    # audio CD quality
-bitsize = -16   # unsigned 16 bit
-channels = 2    # 1 is mono, 2 is stereo
-buffer = 2048   # number of samples (experiment to get right sound)
-pg.mixer.init(freq, bitsize, channels, buffer)
-pg.mixer.music.set_volume(1)
-
-
-def play_music(music_file, wait=True):
-  clock = pg.time.Clock()
-  if pg.mixer.music.get_busy():
-    pg.mixer.music.fadeout(1000)
-    pg.mixer.music.stop()
-  try:
-    pg.mixer.music.load(music_file)
-    print("Music file {} loaded!".format(music_file))
-  except pygame.error:
-    print("File {} not found! {}".format(music_file, pg.get_error()))
-    return
-
-  pg.mixer.music.play()
-
-  # If you want to fade in the audio...
-  # for x in range(0,100):
-  #     pg.mixer.music.set_volume(float(x)/100.0)
-  #     time.sleep(.0075)
-  # # check if playback has finished
-  if (wait):
-    while pg.mixer.music.get_busy():
-      clock.tick(30)
 
 
 def distanz():
@@ -100,7 +60,6 @@ def on_connect(client, userdata, flags, rc):
 
 
 def on_message(client, userdata, msg):
-  global speachLanguage
   logger.debug(msg.topic + " " + msg.payload.decode("utf-8"))
   if "motor/left/speed" in msg.topic:
     motorLeft.speed(float(msg.payload))
@@ -122,38 +81,7 @@ def on_message(client, userdata, msg):
     servoHeadVertical.set_ratio(float(msg.payload))
   elif "servo/head-v/trim" in msg.topic:
     servoHeadVertical.set_trim(float(msg.payload))
-  elif "speach/say" in msg.topic:
-    text = msg.payload.decode("utf-8")
-    textHash = speachLanguage + "-" + hashlib.md5(text.encode("utf-8")).hexdigest()
-    file = "/tmp/speach+"+str(textHash)+".mp3"
-    logger.debug("File for speach: " + file)
-    if not os.path.isfile(file):
-      logger.debug("Downloading speach to: " + file)
-      tts = gTTS(text=text, lang=speachLanguage, slow=True)
-      tts.save(file)
-    play_music(file, False)
-  elif "speach/lang" in msg.topic:
-    speachLanguage = str(msg.payload)
-  elif "music/play/file" in msg.topic:
-    file = str(msg.payload)
-    if os.path.isfile(file):
-      play_music(file, False)
-    else:
-      logger.warn("File not found: " + file)
-  elif "music/stop" in msg.topic:
-    pg.mixer.music.stop()
-  elif "sounds/play/url" in msg.topic:
-    url = str(msg.payload)
-    soundHash = hashlib.md5(url.encode()).hexdigest()
-    file = "/tmp/sound+"+str(soundHash)+".wav"
-    logger.debug("File for sound: " + file)
-    if not os.path.isfile(file):
-      logger.debug("Downloading sound to: " + file)
-      urllib.urlretrieve(url, file)
-    sound = pg.mixer.Sound(file)
-    sound.play()
-  elif "sounds/stop" in msg.topic:
-    pg.mixer.stop()
+
   elif "reset" in msg.topic:
     startup()
 
@@ -170,8 +98,6 @@ def startup():
 
 
 systemRunning = True
-speachLanguage = "de"
-speachCount = 0
 motorLeft = motor.Motor(26, 20, pwm.PwmMotorControl(15))
 motorRight = motor.Motor(13, 16, pwm.PwmMotorControl(14))
 servoHeadVertical = pwm.PwmServoControl(0)
--- control/sounds.py
+++ control/sounds.py
@@ -1,5 +1,19 @@
-import paho.mqtt.client as mqtt
+import hashlib
 import logging
+import os.path
+import urllib
+
+import paho.mqtt.client as mqtt
+import pygame as pg
+from gtts import gTTS
+
+# Init music
+freq = 44100    # audio CD quality
+bitsize = -16   # unsigned 16 bit
+channels = 2    # 1 is mono, 2 is stereo
+buffer = 2048   # number of samples (experiment to get right sound)
+pg.mixer.init(freq, bitsize, channels, buffer)
+pg.mixer.music.set_volume(1)
 
 class SoundsControl:
   def __init__(self):
@@ -8,16 +22,74 @@ class SoundsControl:
     self.client.on_connect = self.on_connect
     self.client.connect("master", 1883, 60)
     self.client.on_message = self.on_message
+    self.speachLanguage = "de"
 
   def on_connect(self, client, userdata, flags, rc):
     self.logger.info("Connected sounds with result code " + str(rc))
-    client.subscribe([("robo/speach/#", 0), ("robo/music/#", 0), ("robo/sounds/#", 0)])
+    client.subscribe(
+        [("robo/speach/#", 0), ("robo/music/#", 0), ("robo/sounds/#", 0)])
 
   def on_message(self, client, userdata, msg):
     self.logger.debug(msg.topic + " " + msg.payload.decode("utf-8"))
+    if "speach/say" in msg.topic:
+      text = msg.payload.decode("utf-8")
+      textHash = self.speachLanguage + "-" + hashlib.md5(
+        text.encode("utf-8")).hexdigest()
+      file = "/tmp/speach+" + str(textHash) + ".mp3"
+      self.logger.debug("File for speach: " + file)
+      if not os.path.isfile(file):
+        self.logger.debug("Downloading speach to: " + file)
+        tts = gTTS(text=text, lang=self.speachLanguage, slow=True)
+        tts.save(file)
+      self.play_music(file, False)
+    elif "speach/lang" in msg.topic:
+      self.speachLanguage = str(msg.payload)
+    elif "music/play/file" in msg.topic:
+      file = str(msg.payload)
+      if os.path.isfile(file):
+        self.play_music(file, False)
+      else:
+        self.logger.warn("File not found: " + file)
+    elif "music/stop" in msg.topic:
+      pg.mixer.music.stop()
+    elif "sounds/play/url" in msg.topic:
+      url = str(msg.payload)
+      soundHash = hashlib.md5(url.encode()).hexdigest()
+      file = "/tmp/sound+" + str(soundHash) + ".wav"
+      self.logger.debug("File for sound: " + file)
+      if not os.path.isfile(file):
+        self.logger.debug("Downloading sound to: " + file)
+        urllib.urlretrieve(url, file)
+      sound = pg.mixer.Sound(file)
+      sound.play()
+    elif "sounds/stop" in msg.topic:
+      pg.mixer.stop()
 
   def start(self):
     self.client.loop_start()
 
   def stop(self):
     self.client.loop_stop(True)
+
+  def play_music(self, music_file, wait=True):
+    clock = pg.time.Clock()
+    if pg.mixer.music.get_busy():
+      pg.mixer.music.fadeout(1000)
+      pg.mixer.music.stop()
+    try:
+      pg.mixer.music.load(music_file)
+      print("Music file {} loaded!".format(music_file))
+    except pygame.error:
+      print("File {} not found! {}".format(music_file, pg.get_error()))
+      return
+
+    pg.mixer.music.play()
+
+    # If you want to fade in the audio...
+    # for x in range(0,100):
+    #     pg.mixer.music.set_volume(float(x)/100.0)
+    #     time.sleep(.0075)
+    # # check if playback has finished
+    if (wait):
+      while pg.mixer.music.get_busy():
+        clock.tick(30)
