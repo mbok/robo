7efeab42deaa68958e99d9f0d1d752854a64e937
mbok <mboook@gmail.com>
2018-09-23 22:31:58 +0200

    update

--- console/console.js
+++ console/console.js
@@ -1,4 +1,4 @@
-new Vue({
+var cc = new Vue({
   el: "#console",
   data: {
     connected: false,
@@ -18,7 +18,8 @@ new Vue({
     this.client.on("message", this.onMessage);
     this.client.on('connect', this.onConnect);
     this.client.on('close', this.onClose);
-    this.client.publish("mqtt/demo", "hello world!")
+
+    this.$on("joystick", this.onJoystick);
   },
   methods: {
     onMessage: function (topic, payload) {
@@ -32,6 +33,30 @@ new Vue({
     },
     publish: function(relativeTopic, payload) {
       this.client.publish("robo/" + relativeTopic, payload+"", {retain: true});
+    },
+    onJoystick: function({event, data}) {
+      if (event.type == "move") {
+        var h = - Math.round((((data.angle.degree % 180) - 90) / 90.0) * 100);
+        var v = Math.min(Math.round(data.distance), 100);
+        this.publish("joystick/h/ratio", h);
+        this.publish("joystick/v/ratio", v);
+        console.log("Joystick change: h x v: ", h, v);
+      } else if (event.type == "end") {
+        this.publish("joystick/h/ratio", 0);
+        this.publish("joystick/v/ratio", 0);
+        console.log("Joystick stop");
+      }
     }
   }
 });
+
+var joystick = nipplejs.create({
+    zone: document.getElementById('joystick'),
+    mode: 'static',
+    position: {left: '50%', top: '50%'},
+    color: 'red',
+    size: 200
+});
+joystick.on('start end pressure move', function(event, data) {
+      cc.$emit("joystick", { "event": event, "data": data });
+    });
